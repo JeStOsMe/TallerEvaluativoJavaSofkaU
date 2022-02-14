190022bab9057e30afc582603bf0e17646f7b414
Jeaniel Osorno <jeanielosorno@gmail.com>
2022-02-13 20:08:58 -0500

    Ejercicio 1 completado.

--- ProyectoEvaluativo/src/Ejercicio1/Ejercicio1.java
+++ ProyectoEvaluativo/src/Ejercicio1/Ejercicio1.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Declara dos variables numéricas (con el valor que desees), e indica cuál es
+ * mayor de los dos. Si son iguales, indicarlo también. Ve cambiando los valores
+ * para comprobar que funcionan.
  */
 package Ejercicio1;
 
@@ -10,5 +10,33 @@ package Ejercicio1;
  * @author Jeaniel Osorno
  */
 public class Ejercicio1 {
+    
+    public static void comparar(double numeroUno, double numeroDos){
+        if (numeroUno > numeroDos){
+            System.out.println("El número " + numeroUno + " es mayor que " + numeroDos);
+        } else if (numeroDos > numeroUno){
+            System.out.println("El número " + numeroDos + " es mayor que " + numeroUno);
+        } else{
+            System.out.println("Los números " + numeroUno + " y " + numeroDos + " son iguales!");
+        }
+    }
+    
+    public static void main(String[] args) {
+        double numeroUno, numeroDos;
+        numeroUno = 3.14;
+        numeroDos = 2;
+        
+        comparar(numeroUno, numeroDos);
+        
+        numeroUno = 8;
+        numeroDos = 23;
+        
+        comparar(numeroUno, numeroDos);
+        
+        numeroUno = 33.7;
+        numeroDos = 33.7;
+        
+        comparar(numeroUno, numeroDos);
+    }
     
 }
