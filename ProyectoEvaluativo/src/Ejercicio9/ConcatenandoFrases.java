c04e0207ab3a160a73dc757ff1cb185485d9906a
Jeaniel Osorno <jeanielosorno@gmail.com>
2022-02-13 22:29:26 -0500

    Ejercicio 9 completado

---
+++ ProyectoEvaluativo/src/Ejercicio9/ConcatenandoFrases.java
@@ -0,0 +1,30 @@
+/*
+ * Del texto "La sonrisa sera la mejor arma contra la tristeza", remplaza todas 
+ * las a del string por e. Adicionalmente, concatenar a esta frase una adicional
+ * ingresada por consola, y luego las muestre unidas.
+ */
+package Ejercicio9;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jeaniel Osorno
+ */
+public class ConcatenandoFrases {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String original, nueva;
+        original = "La sonrisa sera la mejor arma contra la tristeza ";
+        
+        System.out.println("     ***** Concatenando frases *****");
+        System.out.println("Por favor, ingresa una frase (a libre elección) para concatenar: ");
+        nueva = sc.nextLine();
+        
+        System.out.println("La frase original es: " + original);
+        original = original.replace("a", "e");
+        System.out.println("La frase original modificada queda: " + original);
+        System.out.println("Si unimos ambas frases, quedaría: " + original.concat(nueva));
+    }
+}
